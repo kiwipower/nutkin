75e3f971ecd1d098c65e3398cf597a5923c51a26
Alistair Lowe <alowe@kiwipowered.com>
2019-11-06 16:31:18 +0000

    Removed platform-dependant scripts
    Fixed filename case
    
    Fixed more case issues

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p build
-pleasebuild src/nutkin.nut > build/nutkin.nut
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./build.sh
-pleasebuild test/nutkin.spec.nut > build/nutkin.spec.nut
-sq build/nutkin.spec.nut
-sq build/nutkin.spec.nut pattern
